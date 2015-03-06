ddaf61222de1f339070df962a2f67839b1e22110
Leon Rowland <leon@clearvox.nl>
2015-03-06 13:29:25 +0100

    user event action

--- src/Clearvox/Asterisk/AMI/Message/Action/UserEventAction.php
+++ src/Clearvox/Asterisk/AMI/Message/Action/UserEventAction.php
@@ -1,1 +1,11 @@
 <?php
+namespace Clearvox\Asterisk\AMI\Message\Action;
+
+class UserEventAction extends ActionMessage
+{
+    public function __construct($name)
+    {
+        parent::__construct('UserEvent');
+        $this->setKey('UserEvent', $name);
+    }
+}
